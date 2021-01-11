3fbbdda782a3b90955856d9df108ba0f0ebfcef8
Orifiel <orifieleifiro@gmail.com>
2021-01-11 16:24:42 -0300

    Criação da Pagina de Cadastro

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Form } from '@unform/web';
+import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/input';
+import Button from '../../components/button';
+
+import { Container, Content, Background } from './style';
+
+const SignUp: React.FC = () => {
+  function handleSubmit(dados: Object): void {
+    console.log(dados);
+  }
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu Cadastro</h1>
+          <Input name="nome" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para Logon
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
+export default SignUp;
