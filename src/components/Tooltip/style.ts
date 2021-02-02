43e6b3d4d5abb60bf12fa1277c82beddd82e4ac2
CleitonMattos <cleitonmattos87@gmail.com>
2021-02-01 23:30:22 -0300

    Tooltip para as msg de erro

--- src/components/Tooltip/style.ts
+++ src/components/Tooltip/style.ts
@@ -8,9 +8,10 @@ export const Container = styled.div`
     background: #ff9000;
     padding: 8px;
     border-radius: 4px;
+    font-size: 14px;
     font-weight: 500;
     opacity: 0;
-    transition: visibility opacity 0.8s;
+    transition: opacity 0.4s;
     visibility: hidden;
 
     position: absolute;
@@ -24,7 +25,7 @@ export const Container = styled.div`
       content: '';
       border-style: solid;
       border-color: #ff9000 transparent;
-      border-width: 6px 6px 0 6px;
+      border-width: 10px 10px 0 10px;
       top: 100%;
       position: absolute;
       left: 50%;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,41 +1,74 @@
-import React from 'react';
+import React, { useCallback, useRef, useContext } from 'react';
 
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
+import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/input';
 import Button from '../../components/button';
 
 import { Container, Content, Background } from './style';
+import AuthContext from '../../context/AuthContext';
+import getValidationErrors from '../../utils/getValidationErrors';
 
-const SignIn: React.FC = () => (
-  <>
-    <Container>
-      <Content>
-        <img src={logoImg} alt="GoBarber" />
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-        <form>
-          <h1>Faça seu Logon</h1>
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="Senha"
-          />
-          <Button type="submit">Entrar</Button>
-          <a href="forgot">Esqueci minha senha</a>
-        </form>
+  const { name } = useContext(AuthContext);
 
-        <a href="login">
-          <FiLogIn />
-          Criar Conta
-        </a>
-      </Content>
-      <Background />
-    </Container>
-  </>
-);
+  // eslint-disable-next-line no-console
+  console.log(name);
+
+  const handleSubmit = useCallback(async (data: Object) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Email obrigatorio')
+          .email('Digite um email valido'),
+        password: Yup.string().required('Senha obrigatorio'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+  return (
+    <>
+      <Container>
+        <Content>
+          <img src={logoImg} alt="GoBarber" />
+
+          <Form ref={formRef} onSubmit={handleSubmit}>
+            <h1>Faça seu Logon</h1>
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="Senha"
+            />
+            <Button type="submit">Entrar</Button>
+            <a href="forgot">Esqueci minha senha</a>
+          </Form>
+
+          <a href="login">
+            <FiLogIn />
+            Criar Conta
+          </a>
+        </Content>
+        <Background />
+      </Container>
+    </>
+  );
+};
 
 export default SignIn;
